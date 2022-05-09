027b0bc8908d686fcfd03a0e173fa22220abeec5
Frankleiton Levy <frankleitonlevy@mobister.com.br>
2022-05-08 22:23:17 -0300

    fix: mudar senha para servidor

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-  dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: '88877228',
-  database: 'escuta_infantil',
+  dialect: "mysql",
+  host: "localhost",
+  username: "root",
+  password: "Senac@22",
+  database: "escuta_infantil",
   define: {
     timestamps: true,
     underscored: true,
