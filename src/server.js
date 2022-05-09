aece618dff0269b32d607c0ca35178349cafe194
Frankleiton Levy <frankleitonlevy@mobister.com.br>
2022-05-08 21:36:33 -0300

    edit sertificado

--- src/server.js
+++ src/server.js
@@ -12,13 +12,13 @@ const app = express();
 // var jsonPath_key = path.join(__dirname, './', 'certs/cert.pem'); 
 
 
-var options = {
-   key: fs.readFileSync('/etc/letsencrypt/live/www.escutainfantil.online/privkey.pem', 'utf8'),
-   cert: fs.readFileSync('/etc/letsencrypt/live/www.escutainfantil.online/cert.pem', 'utf8'),
-   ca: fs.readFileSync('/etc/letsencrypt/live/www.escutainfantil.online/chain.pem', 'utf8')
-}
+// var options = {
+//    key: fs.readFileSync('/etc/letsencrypt/live/www.escutainfantil.online/privkey.pem', 'utf8'),
+//    cert: fs.readFileSync('/etc/letsencrypt/live/www.escutainfantil.online/cert.pem', 'utf8'),
+//    ca: fs.readFileSync('/etc/letsencrypt/live/www.escutainfantil.online/chain.pem', 'utf8')
+// }
 
-var secure = https.createServer(options, app); // for express
+//var secure = https.createServer(options, app); // for express
 
 app.use(cors({
     origin: '*'
@@ -27,8 +27,8 @@ app.use(cors({
 app.use(express.json());
 app.use(routes);
 
-secure.listen(3001, function() {
-    console.log('localhost started on', 3001)
-})
+// secure.listen(3001, function() {
+//     console.log('localhost started on', 3001)
+// })
 
-//app.listen(3001, () => console.log('Server started on port 3001'));
+app.listen(3001, () => console.log('Server started on port 3001'));
